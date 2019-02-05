6026759d9eeed7546225a186f3faa228c5aebc29
holodnyakdasha <holodnyakdasha@gmail.com>
2019-02-05 18:54:27 +0200

    created cardList and added search by github users name

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {AxiosInstance as axios} from "axios";
+
 
 class App extends Component {
+
+  state = {
+    cards:[]
+  };
+  addNewCard = (cardInfo) =>{
+    this.setState(prevState =>({
+        cards: prevState.cards.concat(cardInfo)
+    }));
+
+};
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+        <div>
+          <Form onSubmit = {this.addNewCard} />
+           <CardList cards={this.state.cards} />
+        </div>
+
     );
   }
 }
 
 export default App;
+
+const Card =(props) => {
+  return(
+      <div style={{margin:'1em'}}>
+        <img src={props.avatar_url}/>
+        <div style={{display:'inline-block', marginLeft:10}}>
+          <div style={{fontSize:'1.25em',fontWeight:'bold'}}>{props.name}</div>
+          <div>{props.company}</div>
+      </div>
+      </div>
+
+  );
+
+};
+
+
+const CardList = (props) =>{
+  return(
+      <div>
+        {props.cards.map(card => <Card {...card} />)}
+      </div>
+  );
+};
+
+
+class Form extends React.Component{
+  state = {userName: ''}
+  handleSubmit = (event)=>{
+    event.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.userName}`).then(resp => {
+          this.props.onSubmit(resp.data);
+          this.setState({userName: ''});
+        });
+
+  };
+  render() {
+    return (
+        <form onSubmit ={this.handleSubmit}>
+          <input type='text'
+                 value={this.state.userName}
+                 onChange={(event)=> this.setState({userName: event.target.value})}
+                 placeholder={"Github user name"} required/>
+          <button type={'submit'}>Add card</button>
+        </form>
+    );
+  }
+
+}
